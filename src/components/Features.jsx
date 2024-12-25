bcc7b8fd66e7372a4bc08029923ced7fd063665a
Anuj Kumar <anujkumarcoder@gmail.com>
2024-12-25 16:41:17 +0530

    l

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -30,36 +30,43 @@ const features = [
 
 const FeatureCard = ({ title, description, icon }) => {
   return (
-    <div className="relative flex flex-col justify-center items-center font-lato bg-[#262626] text-neutral-100 p-6 rounded-[16px] w-[348px] h-[320px] ">
+    <div className="relative flex flex-col justify-center items-center font-lato bg-[#262626] text-neutral-100 p-6 rounded-[16px] w-full sm:w-[348px] h-[320px] ">
       {/* Circular Image Container */}
-      <div className="absolute -top-10   flex items-center justify-center">
-        <Image src={icon} alt={title} className="w-[130px] h-[130px] object-contain flex justify-center" />
+      <div className="absolute -top-10 flex items-center justify-center">
+        <Image
+          src={icon}
+          alt={title}
+          className="w-[80px] h-[80px] sm:w-[130px] sm:h-[130px] object-contain flex justify-center"
+        />
       </div>
-      <h3 className=" text-neutral-300 text-[29px] font-lato w-[260px] font-bold flex justify-center">{title}</h3>
-      <p className="text-neutral-300 text-center font-lato text-[20px] mt-2 flex justify-center">{description}</p>
+      <h3 className="text-neutral-300 text-[20px] sm:text-[29px] font-lato w-[260px] font-bold flex justify-center mt-20 sm:mt-0">
+        {title}
+      </h3>
+      <p className="text-neutral-300 text-center font-lato text-[16px] sm:text-[20px] mt-2 flex justify-center">
+        {description}
+      </p>
     </div>
   );
 };
 
 const FeaturesSection = () => {
-    return (
-      <section className="bg-neutral-900 py-16 px-4">
-        <h2 className="text-center text-4xl md:text-6xl font-bold text-neutral-100 mb-12">
-          Key Features
-        </h2>
-        <div className="flex flex-wrap justify-center items-center gap-8">
-          {features.map((feature, index) => (
-            <FeatureCard
-              key={index}
-              title={feature.title}
-              description={feature.description}
-              icon={feature.icon}
-            />
-          ))}
-        </div>
-      </section>
-    );
-  };
-  
+  return (
+    <section className="bg-neutral-900 py-16 px-4">
+      <h2 className="text-center text-3xl sm:text-4xl md:text-6xl font-bold text-neutral-100 mb-12">
+        Key Features
+      </h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 justify-center items-center">
+        {features.map((feature, index) => (
+          <FeatureCard
+            key={index}
+            title={feature.title}
+            description={feature.description}
+            icon={feature.icon}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
 
 export default FeaturesSection;
