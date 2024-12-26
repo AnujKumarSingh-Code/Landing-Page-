d8f842cb23dbf3dfd737f9e50a1968bd725c0864
Anuj Kumar <anujkumarcoder@gmail.com>
2024-12-26 17:00:10 +0530

    dd

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -100,7 +100,7 @@ const Header = () => {
           </div>
         </div>
 
-        <div className="relative mt-40 ml-56 ">
+        <div className="relative mt-40 ml-48 ">
           <Image src={A} alt="A" className=" w-[550px] h-[800px] mt-[-100px]  " />
         </div>
       </div>
